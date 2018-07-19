281f96f4bcb65d3bf833ff4d3c38f4762f0ec9aa
kononovm <govardrorkmk@gmail.com>
2018-07-19 18:44:37 +0300

    Добавил сайт по вебинару

---
+++ app/js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+$('.scroll').click( function(){
+  var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+        }
+      return false;
+})
+
+$(".mask").mask("+7 (999) 999-99-99");
+
+});
